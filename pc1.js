c29bc7c7c63e7eb5a07dad5dbd931155edcda752
mr-nobody11 <aswinvs542@gmail.com>
2023-05-23 14:14:41 +0530

    j

---
+++ pc1.js
@@ -0,0 +1,15 @@
+function hello()
+{
+let but=document.getElementById('p1').value;
+
+let but1=document.getElementById('p2').value;
+
+if(but != but1)
+{
+  //  but.addEventListener('click',display)
+    alert(' Password does not Match');
+}
+else
+{
+    alert(' Password  match');
+}}
